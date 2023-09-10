e7d0723ca31a36a6a451cd2d863608945bdaafdf
patelsweta2 <sweta0442@gmail.com>
2023-09-10 20:23:12 +0530

    Changes

---
+++ client/src/components/Prices.js
@@ -0,0 +1,27 @@
+export const Prices = [
+  {
+    _id: 0,
+    name: " ₹‎0 to 99",
+    array: [0, 99],
+  },
+  {
+    _id: 1,
+    name: " ₹‎100 to 499",
+    array: [100, 499],
+  },
+  {
+    _id: 2,
+    name: " ₹‎500 to 999",
+    array: [500, 999],
+  },
+  {
+    _id: 3,
+    name: " ₹‎1000 to 2999",
+    array: [1000, 2999],
+  },
+  {
+    _id: 4,
+    name: " ₹‎3000 or more",
+    array: [3000, 10000],
+  },
+];
